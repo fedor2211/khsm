4548c0993d927368b199bab7a1528515779daaec
Gwarek2 <fedorsah@gmail.com>
2023-01-24 21:55:27 +0300

    Add tests for game_question helpers methods

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -10,31 +10,45 @@ RSpec.describe GameQuestion, type: :model do
   # она будет создана на фабрике заново для каждого блока it, где она вызывается
   let(:game_question) { FactoryBot.create(:game_question, a: 2, b: 1, c: 4, d: 3) }
 
-  # группа тестов на игровое состояние объекта вопроса
-  context 'game status' do
-    it 'correct #text' do
+  describe '#text' do
+    it 'returns question text' do
       expect(game_question.text).to eq(game_question.question.text)
     end
+  end
 
-    it 'correct #level' do
+  describe '#level' do
+    it 'returns question level' do
       expect(game_question.level).to eq(game_question.question.level)
     end
+  end
 
-    # тест на правильную генерацию хэша с вариантами
-    it 'correct #variants' do
+  # тест на правильную генерацию хэша с вариантами
+  describe '#variants' do
+    it 'returns question variants hash' do
       expect(game_question.variants).to eq({'a' => game_question.question.answer2,
                                             'b' => game_question.question.answer1,
                                             'c' => game_question.question.answer4,
                                             'd' => game_question.question.answer3})
     end
+  end
 
-    it 'correct #correct_answer_key' do
+  describe '#correct_answer_key' do
+    it 'returns key for correct answer' do
       expect(game_question.correct_answer_key).to eq 'b'
     end
+  end
 
-    it 'correct #answer_correct?' do
-      # именно под буквой b в тесте мы спрятали указатель на верный ответ
-      expect(game_question.answer_correct?('b')).to be_truthy
+  describe '#answer_correct?' do
+    context 'when answer is correct' do
+      it 'returns true' do
+        expect(game_question.answer_correct?('b')).to be_truthy
+      end
+    end
+
+    context 'when answer is incorrect' do
+      it 'returns false' do
+        expect(game_question.answer_correct?('a')).to be_falsey
+      end
     end
   end
 
@@ -46,16 +60,65 @@ RSpec.describe GameQuestion, type: :model do
   # }
   #
 
-  context 'user helpers' do
-    it 'correct audience_help' do
-      expect(game_question.help_hash).not_to include(:audience_help)
+  describe '#add_audience_help' do
+    before { game_question.add_audience_help }
 
-      game_question.add_audience_help
-
+    it 'creates audience_help entry' do
       expect(game_question.help_hash).to include(:audience_help)
+    end
 
-      ah = game_question.help_hash[:audience_help]
-      expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+    it 'entry is a hash' do
+      expect(game_question.help_hash[:audience_help]).to be_a(Hash)
+    end
+
+    it 'entry contains all variants keys' do
+      expect(
+        game_question.help_hash[:audience_help].keys
+      ).to contain_exactly('a', 'b', 'c', 'd')
+    end
+  end
+
+  describe '#add_friend_call' do
+    before { game_question.add_friend_call }
+
+    it 'creates help_hash_entry' do
+      expect(game_question.help_hash).to include(:friend_call)
+    end
+
+    it 'entry is a string' do
+      expect(game_question.help_hash[:friend_call]).to be_a(String)
+    end
+
+    it 'entry string is not empty' do
+      expect(game_question.help_hash[:friend_call]).not_to be_empty
+    end
+  end
+
+  describe '#add_fifty_fifty' do
+    before { game_question.add_fifty_fifty }
+
+    it 'creates help_hash entry' do
+      expect(game_question.help_hash).to include(:fifty_fifty)
+    end
+
+    it 'entry is an array' do
+      expect(game_question.help_hash[:fifty_fifty]).to be_a Array
+    end
+
+    it 'entry contains half of variants' do
+      expect(game_question.help_hash[:fifty_fifty].size).to eq(2)
+    end
+
+    it 'entry contains correct answer' do
+      expect(
+        game_question.help_hash[:fifty_fifty]
+      ).to include(game_question.correct_answer_key)
+    end
+  end
+
+  describe '#help_hash' do
+    it 'returns hash' do
+      expect(game_question.help_hash).to be_a(Hash)
     end
   end
 end
